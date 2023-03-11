1e9b79c4e3078b76e46cbf98f4a55da246d8e9df
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-03-11 08:11:58 +0545

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,21 @@
+n = int(input('enter the lenght of the sequence:'))
+i = 1
+j = 1
+
+
+print(i)
+print(j)
+
+
+for each in range(n - 2):
+    k = i + j
+    print(k)
+    i = j
+    j = k
+
+
+
+
+
+
+
